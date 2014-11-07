f98f69d95465b45eaa8c5504a18d224757109596
justjavac <justjavac@gmail.com>
2014-11-07 15:38:48 +0800

    format

--- js/main.js
+++ js/main.js
@@ -9,26 +9,30 @@
  * @license GPLv3
  **/
 
-(function () {
-  var head = document.getElementsByTagName('head')[0];
+(function() {
+    var head = document.getElementsByTagName('head')[0];
 
-  if (head) {
-    var script = document.createElement('script');
-    script.type = 'application/x-js';
-    script.src = chrome.extension.getURL('js/detector.js');
+    if (head) {
+        var script = document.createElement('script');
+        script.type = 'application/x-js';
+        script.src = chrome.extension.getURL('js/detector.js');
 
-    var meta = document.createElement('meta');
-    meta.name = 'chromesniffer';
-    meta.id = 'chromesniffer_meta';
-    head.appendChild(meta);
-    head.appendChild(script);
+        var meta = document.createElement('meta');
+        meta.name = 'chromesniffer';
+        meta.id = 'chromesniffer_meta';
+        head.appendChild(meta);
+        head.appendChild(script);
 
-    meta.addEventListener('ready', function () {
-      var apps = JSON.parse(meta.content);
+        meta.addEventListener('ready',
+        function() {
+            var apps = JSON.parse(meta.content);
 
-      if (Object.keys(apps).length > 0) {
-        chrome.extension.sendMessage({msg: "result", apps: apps});
-      }
-    });
-  }
+            if (Object.keys(apps).length > 0) {
+                chrome.extension.sendMessage({
+                    msg: "result",
+                    apps: apps
+                });
+            }
+        });
+    }
 })();
